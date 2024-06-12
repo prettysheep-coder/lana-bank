49d54951af3b70ffdf1a63292563f12a51bb3029
Nicolas Burtey <nb@galoy.io>
2024-06-12 15:59:40 -0600

    feat: using userId from auth, not graphql input

--- core/src/server/public/graphql/schema.rs
+++ core/src/server/public/graphql/schema.rs
@@ -4,7 +4,10 @@ use super::{fixed_term_loan::*, user::*};
 use crate::{
     app::LavaApp,
     primitives::{FixedTermLoanId, UserId},
-    server::shared_graphql::{fixed_term_loan::FixedTermLoan, primitives::UUID, user::User},
+    server::{
+        public::ClientContext,
+        shared_graphql::{fixed_term_loan::FixedTermLoan, primitives::UUID, user::User},
+    },
 };
 
 pub struct Query;
@@ -29,49 +32,57 @@ impl Query {
         let user = app.users().find_by_id(UserId::from(id)).await?;
         Ok(user.map(User::from))
     }
+
+    async fn me(&self, ctx: &Context<'_>) -> async_graphql::Result<Option<User>> {
+        let client_id = match ctx.data_unchecked::<ClientContext>().client_id {
+            None => return Err("Unauthorized".into()),
+            Some(id) => UserId::from(id),
+        };
+
+        let app = ctx.data_unchecked::<LavaApp>();
+        let user = app.users().find_by_id(client_id).await?;
+
+        Ok(user.map(User::from))
+    }
 }
 
 pub struct Mutation;
 
 #[Object]
 impl Mutation {
-    pub async fn user_create(
-        &self,
-        ctx: &Context<'_>,
-        input: UserCreateInput,
-    ) -> async_graphql::Result<UserCreatePayload> {
-        let app = ctx.data_unchecked::<LavaApp>();
-        let user = app.users().create_user(input.bitfinex_username).await?;
-        Ok(UserCreatePayload::from(user))
-    }
-
     pub async fn withdrawal_initiate(
         &self,
         ctx: &Context<'_>,
         input: WithdrawalInitiateInput,
     ) -> async_graphql::Result<WithdrawalInitiatePayload> {
+        let client_id = match ctx.data_unchecked::<ClientContext>().client_id {
+            None => return Err("Unauthorized".into()),
+            Some(id) => UserId::from(id),
+        };
+
         let app = ctx.data_unchecked::<LavaApp>();
-        Ok(WithdrawalInitiatePayload::from(
-            app.withdraws()
-                .initiate(
-                    input.user_id,
-                    input.amount,
-                    input.destination,
-                    input.reference,
-                )
-                .await?,
-        ))
+
+        let withdraw = app
+            .withdraws()
+            .initiate(client_id, input.amount, input.destination, input.reference)
+            .await?;
+
+        Ok(WithdrawalInitiatePayload::from(withdraw))
     }
 
     pub async fn fixed_term_loan_create(
         &self,
         ctx: &Context<'_>,
-        input: FixedTermLoanCreateInput,
     ) -> async_graphql::Result<FixedTermLoanCreatePayload> {
+        let client_id = match ctx.data_unchecked::<ClientContext>().client_id {
+            None => return Err("Unauthorized".into()),
+            Some(id) => UserId::from(id),
+        };
+
         let app = ctx.data_unchecked::<LavaApp>();
         let loan = app
             .fixed_term_loans()
-            .create_loan_for_user(input.user_id)
+            .create_loan_for_user(client_id)
             .await?;
         Ok(FixedTermLoanCreatePayload::from(loan))
     }
@@ -81,6 +92,8 @@ impl Mutation {
         ctx: &Context<'_>,
         input: FixedTermLoanApproveInput,
     ) -> async_graphql::Result<FixedTermLoanApprovePayload> {
+        // TODO: validate userId
+
         let app = ctx.data_unchecked::<LavaApp>();
         let loan = app
             .fixed_term_loans()
--- core/src/server/public/mod.rs
+++ core/src/server/public/mod.rs
@@ -2,10 +2,11 @@ pub mod graphql;
 
 mod config;
 
-use async_graphql::*;
+use async_graphql::{EmptySubscription, Schema};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{routing::get, Extension, Router};
 use axum_extra::headers::HeaderMap;
+use uuid::Uuid;
 
 use crate::app::LavaApp;
 
@@ -29,6 +30,16 @@ pub async fn run(config: PublicServerConfig, app: LavaApp) -> anyhow::Result<()>
     Ok(())
 }
 
+pub struct ClientContext {
+    pub client_id: Option<Uuid>,
+}
+
+impl ClientContext {
+    pub fn new(client_id: Option<Uuid>) -> Self {
+        ClientContext { client_id }
+    }
+}
+
 pub async fn graphql_handler(
     headers: HeaderMap,
     schema: Extension<Schema<graphql::Query, graphql::Mutation, EmptySubscription>>,
@@ -36,7 +47,15 @@ pub async fn graphql_handler(
 ) -> GraphQLResponse {
     lava_tracing::http::extract_tracing(&headers);
     let req = req.into_inner();
-    schema.execute(req).await.into()
+
+    let client_id = headers
+        .get("X-CLIENT-ID")
+        .and_then(|header| header.to_str().ok())
+        .and_then(|id_str| Uuid::parse_str(id_str).ok());
+
+    let context = ClientContext::new(client_id);
+
+    schema.execute(req.data(context)).await.into()
 }
 
 async fn playground() -> impl axum::response::IntoResponse {
