62857ecf592e82b0c99987ce064fadafcd2914ba
siddharth <siddharthtiwarikreplind@gmail.com>
2024-07-09 13:51:54 +0530

    test: added in github actions

--- apps/customer-portal/cypress.config.ts
+++ apps/customer-portal/cypress.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "cypress"
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4455",
-    setupNodeEvents(on, config) {
-      console.log(on, config)
-    },
+    screenshotOnRunFailure: false,
   },
 })
