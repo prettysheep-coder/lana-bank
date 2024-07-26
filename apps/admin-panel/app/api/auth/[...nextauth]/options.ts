d3a321554b24d173dfff948708204bea1a0b27d6
siddharth <siddharthtiwarikreplind@gmail.com>
2024-07-26 14:43:53 +0530

    chore: signin using magic link

--- apps/admin-panel/app/api/auth/[...nextauth]/options.ts
+++ apps/admin-panel/app/api/auth/[...nextauth]/options.ts
@@ -1,49 +1,56 @@
-import CredentialsProvider from "next-auth/providers/credentials"
+import EmailProvider from "next-auth/providers/email"
+import PostgresAdapter from "@auth/pg-adapter"
 
-import { CallbacksOptions } from "next-auth"
+import { Pool } from "pg"
+import { NextAuthOptions } from "next-auth"
+import { Adapter } from "next-auth/adapters"
 
-const providers = []
-
-providers.push(
-  CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      username: { label: "Username", type: "text" },
-      password: { label: "Password", type: "password" },
-    },
-    authorize: async (credentials) => {
-      if (credentials?.username === "admin" && credentials?.password === "admin") {
-        return { id: "1", name: "admin", email: "admin@galoy.io" }
-      }
-      if (credentials?.username === "user" && credentials?.password === "user") {
-        return { id: "2", name: "user", email: "user@galoy.io" }
-      }
-      return null
-    },
-  }),
-)
-
-const callbacks: Partial<CallbacksOptions> = {
-  async signIn({ account, profile, user }) {
-    if (account?.provider === "credentials") {
-      return Boolean(user)
-    }
-
-    if (!account || !profile) {
-      return false
-    }
-
-    const email = profile?.email
-    if (!email) {
-      return false
-    }
-
-    const verified = "email_verified" in profile && profile.email_verified
-    return Boolean(verified)
+const allowedUsers = [
+  {
+    id: 1,
+    name: "Admin",
+    email: "admin@lava.io",
+    role: "admin",
   },
-}
+  {
+    id: 2,
+    name: "User",
+    email: "user@lava.io",
+    role: "user",
+  },
+]
 
-export const authOptions = {
-  providers,
-  callbacks,
+const pool = new Pool({
+  connectionString: process.env.NEXT_AUTH_DATABASE_URL,
+})
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ account }) {
+      const email = account?.providerAccountId
+      const user = allowedUsers.find((user) => user.email === email)
+      if (account?.provider === "email" && email && user) {
+        return true
+      }
+      return false
+    },
+    async session({ session, token }) {
+      const user = allowedUsers.find((allowedUser) => allowedUser.email === token.email)
+      if (session.user) {
+        session.user.name = user?.name
+        session.user.email = token.email
+      }
+      return session
+    },
+  },
+  adapter: PostgresAdapter(pool) as Adapter,
 }
