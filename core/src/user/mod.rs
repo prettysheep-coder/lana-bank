2b8544ef1695d31a341df6c4636d19254b0859e1
Vaibhav <thevaibhavdixit@gmail.com>
2024-07-28 23:45:58 +0530

    chore: impl uses cases for Users service

--- core/src/user/mod.rs
+++ core/src/user/mod.rs
@@ -3,7 +3,10 @@ mod entity;
 pub mod error;
 mod repo;
 
-use crate::{authorization::Authorization, primitives::Role};
+use crate::{
+    authorization::{Action, Authorization, Object, UserAction},
+    primitives::{Role, Subject, UserId},
+};
 
 pub use config::*;
 pub use entity::*;
@@ -24,22 +27,22 @@ impl Users {
         config: UserConfig,
     ) -> Result<Self, UserError> {
         let repo = UserRepo::new(pool);
-        let mut users = Self {
+        let users = Self {
             pool: pool.clone(),
             authz: authz.clone(),
             repo,
         };
 
         if let Some(email) = config.super_user_email {
-            users.create_super_user(email).await?;
+            users.create_and_assign_role_to_super_user(email).await?;
         }
 
         Ok(users)
     }
 
-    async fn create_super_user(&mut self, email: String) -> Result<(), UserError> {
+    async fn create_and_assign_role_to_super_user(&self, email: String) -> Result<(), UserError> {
         if self.find_by_email(&email).await?.is_none() {
-            self.create_user(&email).await?;
+            self.create_super_user(&email).await?;
             self.authz
                 .assign_role_to_subject(email, &Role::SuperUser)
                 .await?;
@@ -51,7 +54,25 @@ impl Users {
         &self.repo
     }
 
-    pub async fn create_user(&self, email: impl Into<String>) -> Result<User, UserError> {
+    pub async fn create_super_user(&self, email: impl Into<String>) -> Result<User, UserError> {
+        let new_user = NewUser::builder()
+            .email(email)
+            .build()
+            .expect("Could not build user");
+        let mut db = self.pool.begin().await?;
+        let user = self.repo.create_in_tx(&mut db, new_user).await?;
+        db.commit().await?;
+        Ok(user)
+    }
+
+    pub async fn create_user(
+        &self,
+        sub: &Subject,
+        email: impl Into<String>,
+    ) -> Result<User, UserError> {
+        self.authz
+            .check_permission(sub, Object::User, Action::User(UserAction::Create))
+            .await?;
         let new_user = NewUser::builder()
             .email(email)
             .build()
@@ -68,5 +89,43 @@ impl Users {
             Err(UserError::CouldNotFindByEmail(_)) => Ok(None),
             Err(e) => Err(e),
         }
+    }
+
+    pub async fn list_users(&self, sub: &Subject) -> Result<Vec<User>, UserError> {
+        self.authz
+            .check_permission(sub, Object::User, Action::User(UserAction::List))
+            .await?;
+        Ok(self.repo.list().await?)
+    }
+
+    pub async fn assign_role_to_user(
+        &self,
+        sub: &Subject,
+        id: UserId,
+        role: Role,
+    ) -> Result<User, UserError> {
+        let id = id.into();
+        self.authz
+            .check_permission(sub, Object::User, Action::User(UserAction::AssignRole))
+            .await?;
+        let user = self.repo.find_by_id(id).await?;
+        let email = user.email.clone();
+        self.authz.assign_role_to_subject(email, &role).await?;
+        Ok(user)
+    }
+
+    pub async fn revoke_role_from_user(
+        &self,
+        sub: &Subject,
+        id: UserId,
+        role: &Role,
+    ) -> Result<User, UserError> {
+        self.authz
+            .check_permission(sub, Object::User, Action::User(UserAction::RevokeRole))
+            .await?;
+        let user = self.repo.find_by_id(id).await?;
+        let email = user.email.clone();
+        self.authz.revoke_role_from_subject(email, role).await?;
+        Ok(user)
     }
 }
