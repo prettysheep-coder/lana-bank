ea4d2ce68ca02a0090b340503ddc9d7db60eaf33
Nicolas Burtey <nb@galoy.io>
2024-08-07 13:40:24 +0200

    chore: better error naming

--- core/src/audit/error.rs
+++ core/src/audit/error.rs
@@ -4,6 +4,6 @@ use thiserror::Error;
 pub enum AuditError {
     #[error("AuditError - Sqlx: {0}")]
     Sqlx(#[from] sqlx::Error),
-    #[error("AuditError - TryFromIntError: {0}")]
-    TryFromIntError(#[from] std::num::TryFromIntError),
+    #[error("AuditError - ParseCursorError: {0}")]
+    ParseCursorError(#[from] std::num::TryFromIntError),
 }
