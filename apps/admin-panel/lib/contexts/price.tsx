1f75864a3ccb1d39ba96a30ba6cca2f09f0c3a3a
sandipndev <hey@sandipan.dev>
2024-08-22 13:28:21 +0530

    chore: add price context

---
+++ apps/admin-panel/lib/contexts/price.tsx
@@ -0,0 +1,33 @@
+import { createContext, useCallback, useContext, useState } from "react"
+
+type PriceContextType = {
+  price: number
+  satsToCents: null | ((sats: number) => number)
+}
+
+const initialPriceValues: PriceContextType = {
+  price: 60000_00, // 1BTC = 6000000 cents
+  satsToCents: null,
+}
+
+const PriceContext = createContext(initialPriceValues)
+const PriceProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  // TODO: some subscription that sets the price
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [price, _setPrice] = useState(initialPriceValues.price)
+
+  const satsToCents = useCallback(
+    (sats: number) => sats * (1 / 100000000) * price,
+    [price],
+  )
+
+  return (
+    <PriceContext.Provider value={{ price, satsToCents }}>
+      {children}
+    </PriceContext.Provider>
+  )
+}
+
+const usePrice = () => useContext(PriceContext)
+
+export { PriceProvider, usePrice }
