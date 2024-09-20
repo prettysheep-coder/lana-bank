6b2fb580ba6e308f2be98ff7dc12f85c17b565f3
bodymindarts <justin@galoy.io>
2024-09-20 14:51:47 +0200

    fix: missing data_export/error.rs

--- core/src/data_export/error.rs
+++ core/src/data_export/error.rs
@@ -3,5 +3,5 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum ExportError {
     #[error("ExportError - CalaError: {0}")]
-    CalaError(#[from] super::cala::error::CalaError),
+    Cala(#[from] super::cala::error::CalaError),
 }
