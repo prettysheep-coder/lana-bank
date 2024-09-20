dadcaebdc8e1a49dbb147e87d1b0cecb1a95eada
bodymindarts <justin@galoy.io>
2024-09-20 09:57:01 +0200

    fix: sumsub_applicants import

--- tf/import.tf
+++ tf/import.tf
@@ -5,12 +5,17 @@ locals {
     "loan_events",
     "withdraw_events",
     "deposit_events",
-    "sumsub_applicants",
   ] : []
 }
 
 import {
   for_each = local.setup_bq ? toset(local.bq_tables) : []
   to       = module.setup.google_bigquery_table.entities[each.value]
+  id       = "projects/${local.project_id}/datasets/${local.dataset_id}/tables/${each.value}"
+}
+
+import {
+  for_each = local.setup_bq ? ["sumsub_applicants"] : []
+  to       = module.setup.google_bigquery_table.sumsub_applicants[0]
   id       = "projects/${local.project_id}/datasets/${local.dataset_id}/tables/${each.value}"
 }
