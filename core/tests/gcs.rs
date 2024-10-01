832a73203be5f1f8c49dc855f6073440dc7f46bd
bodymindarts <justin@galoy.io>
2024-10-01 11:32:40 +0200

    fix: test compilation

--- core/tests/gcs.rs
+++ core/tests/gcs.rs
@@ -1,23 +1,22 @@
 mod helpers;
 
 use lava_core::{
-    cli::config::Config,
+    service_account::ServiceAccountConfig,
     storage::{config::StorageConfig, Storage},
 };
 
 #[tokio::test]
 async fn upload_doc() -> anyhow::Result<()> {
-    let sa_creds_base64 = if let Ok( sa_creds_base64) = std::env::var("SA_CREDS_BASE64") {
+    let sa_creds_base64 = if let Ok(sa_creds_base64) = std::env::var("SA_CREDS_BASE64") {
         sa_creds_base64
     } else {
         return Ok(());
-    }
-
+    };
 
     let sa = ServiceAccountConfig::default().set_sa_creds_base64(sa_creds_base64)?;
 
     let config = if let Ok(name_prefix) = std::env::var("DEV_ENV_NAME_PREFIX") {
-        StorageConfig::new_dev_mode(name_prefix, sa);
+        StorageConfig::new_dev_mode(name_prefix, sa)
     } else {
         StorageConfig {
             service_account: Some(sa),
@@ -25,7 +24,7 @@ async fn upload_doc() -> anyhow::Result<()> {
         }
     };
 
-    let storage = Storage::new(config);
+    let storage = Storage::new(&config);
 
     let file = "test".as_bytes().to_vec();
     let filename = "test.txt";
