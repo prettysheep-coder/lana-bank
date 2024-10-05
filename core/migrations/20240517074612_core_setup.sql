1e491a094fc0585ab58d8ed668dbee030c5e55c2
Nicolas Burtey <nb@galoy.io>
2024-10-05 17:10:16 -0400

    chore: sumsub callback to return 200 when sandbox mode is true

--- core/migrations/20240517074612_core_setup.sql
+++ core/migrations/20240517074612_core_setup.sql
@@ -187,7 +187,7 @@ CREATE TABLE audit_entries (
 
 CREATE TABLE sumsub_callbacks (
   id BIGSERIAL PRIMARY KEY,
-  customer_id UUID NOT NULL REFERENCES customers(id),
+  customer_id UUID NOT NULL, -- REFERENCES customers(id) -- not enforced to get all callbacks
   content JSONB NOT NULL,
   recorded_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
