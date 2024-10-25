cdb6f7303088a70d9f9f8e1bb6a4f0713b7c4212
bodymindarts <justin@galoy.io>
2024-10-25 10:18:52 +0200

    chore: add core/audit

---
+++ core/audit/src/cursor.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+use super::AuditEntry;
+use crate::primitives::AuditEntryId;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AuditCursor {
+    pub(super) id: AuditEntryId,
+}
+
+impl<S, O, A> From<&AuditEntry<S, O, A>> for AuditCursor {
+    fn from(entry: &AuditEntry<S, O, A>) -> Self {
+        Self { id: entry.id }
+    }
+}
+
+impl std::fmt::Display for AuditCursor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.id)
+    }
+}
+
+impl std::str::FromStr for AuditCursor {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let id = s.parse::<i64>()?;
+        Ok(AuditCursor {
+            id: AuditEntryId::from(id),
+        })
+    }
+}
