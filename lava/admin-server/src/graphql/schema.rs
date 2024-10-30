5a98b3588af623d50336bb4aed2135180c7f623b
bodymindarts <justin@galoy.io>
2024-10-30 13:56:36 +0100

    chore: users

--- lava/admin-server/src/graphql/schema.rs
+++ lava/admin-server/src/graphql/schema.rs
@@ -18,6 +18,36 @@ impl Query {
         loader.feed_one(user.id, User::from(user.clone())).await;
         Ok(AuthenticatedSubject::from(user))
     }
+
+    async fn user(&self, ctx: &Context<'_>, id: UUID) -> async_graphql::Result<Option<User>> {
+        let app = ctx.data_unchecked::<LavaApp>();
+        let AdminAuthContext { sub } = ctx.data()?;
+        if let Some(user) = app.users().find_by_id(sub, UserId::from(id)).await? {
+            let user = User::from(user);
+            let loader = ctx.data_unchecked::<LavaDataLoader>();
+            loader.feed_one(user.entity.id, user.clone()).await;
+            Ok(Some(user))
+        } else {
+            Ok(None)
+        }
+    }
+
+    async fn users(&self, ctx: &Context<'_>) -> async_graphql::Result<Vec<User>> {
+        let app = ctx.data_unchecked::<LavaApp>();
+        let AdminAuthContext { sub } = ctx.data()?;
+        let loader = ctx.data_unchecked::<LavaDataLoader>();
+        let users: Vec<_> = app
+            .users()
+            .list_users(sub)
+            .await?
+            .into_iter()
+            .map(User::from)
+            .collect();
+        loader
+            .feed_many(users.iter().map(|u| (u.entity.id, u.clone())))
+            .await;
+        Ok(users)
+    }
 }
 
 pub struct Mutation;
