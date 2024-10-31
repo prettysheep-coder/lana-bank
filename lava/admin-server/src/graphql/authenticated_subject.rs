b0776e87d714c4fc1320cd5ee1b98031e0fe1080
bodymindarts <justin@galoy.io>
2024-10-31 14:04:01 +0100

    chore: rename AuthenticatedSubject -> Subject in schea

--- lava/admin-server/src/graphql/authenticated_subject.rs
+++ lava/admin-server/src/graphql/authenticated_subject.rs
@@ -1,33 +1,29 @@
 use async_graphql::*;
 
-use lava_app::{app::LavaApp, authorization::VisibleNavigationItems, user::User as DomainUser};
+use super::user::User;
+use lava_app::{authorization::VisibleNavigationItems, user::User as DomainUser};
 
 use crate::primitives::*;
 
+#[derive(SimpleObject)]
+#[graphql(name = "Subject", complex)]
 pub struct AuthenticatedSubject {
-    entity: Arc<DomainUser>,
+    user: User,
 }
 
-#[Object]
-#[graphql(name = "Subject")]
+#[ComplexObject]
 impl AuthenticatedSubject {
-    async fn user(&self) -> super::user::User {
-        Arc::clone(&self.entity).into()
-    }
-
     async fn visible_navigation_items(
         &self,
         ctx: &Context<'_>,
     ) -> async_graphql::Result<VisibleNavigationItems> {
-        let app = ctx.data_unchecked::<LavaApp>();
-        let AdminAuthContext { sub } = ctx.data()?;
+        let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
         let permissions = app.get_visible_nav_items(sub).await?;
         Ok(permissions)
     }
 
-    async fn subject_create_customer(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
-        let app = ctx.data_unchecked::<LavaApp>();
-        let AdminAuthContext { sub } = ctx.data()?;
+    async fn subject_can_create_customer(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
+        let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
         Ok(app
             .customers()
             .subject_can_create_customer(sub, false)
@@ -35,18 +31,20 @@ impl AuthenticatedSubject {
             .is_ok())
     }
 
-    async fn subject_create_user(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
-        let app = ctx.data_unchecked::<LavaApp>();
-        let AdminAuthContext { sub } = ctx.data()?;
-        Ok(app.users().subject_create_user(sub, false).await.is_ok())
+    async fn subject_can_create_user(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
+        let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
+        Ok(app
+            .users()
+            .subject_can_create_user(sub, false)
+            .await
+            .is_ok())
     }
 
     async fn subject_can_create_terms_template(
         &self,
         ctx: &Context<'_>,
     ) -> async_graphql::Result<bool> {
-        let app = ctx.data_unchecked::<LavaApp>();
-        let AdminAuthContext { sub } = ctx.data()?;
+        let (app, sub) = crate::app_and_sub_from_ctx!(ctx);
         Ok(app
             .terms_templates()
             .subject_can_create_terms_template(sub, false)
@@ -57,6 +55,8 @@ impl AuthenticatedSubject {
 
 impl From<Arc<DomainUser>> for AuthenticatedSubject {
     fn from(entity: Arc<DomainUser>) -> Self {
-        Self { entity }
+        Self {
+            user: User::from(entity),
+        }
     }
 }
