40cb4b4392b86cd37a456161b57c97e374406252
bodymindarts <justin@galoy.io>
2024-11-01 10:28:40 +0100

    chore: add AsMutableEntity

--- lib/es-entity/src/traits.rs
+++ lib/es-entity/src/traits.rs
@@ -21,3 +21,21 @@ pub trait EsEntity: TryFromEvents<Self::Event> {
     fn events_mut(&mut self) -> &mut EntityEvents<Self::Event>;
     fn events(&self) -> &EntityEvents<Self::Event>;
 }
+
+pub trait AsMutableEntity {
+    type Entity: EsEntity;
+    fn mutable(&self) -> Self::Entity;
+}
+
+impl<T, E> AsMutableEntity for T
+where
+    T: EsEntity<Event = E>,
+    E: EsEvent + Clone,
+{
+    type Entity = T;
+
+    fn mutable(&self) -> Self::Entity {
+        <T as TryFromEvents<E>>::try_from_events(self.events().clone())
+            .expect("Could not convert events to entity")
+    }
+}
