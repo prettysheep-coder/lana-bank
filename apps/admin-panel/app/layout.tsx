2ac3c86c2944dc1ce167f81a4be3bc67b5a85ea9
sandipndev <hey@sandipan.dev>
2024-11-06 01:10:58 +0530

    save

--- apps/admin-panel/app/layout.tsx
+++ apps/admin-panel/app/layout.tsx
@@ -2,24 +2,29 @@
 import "@/lib/ui/globals.css"
 import type { Metadata } from "next"
 
+import { HelveticaNeueFont, RobotoMono } from "@/lib/ui/fonts"
+import { Toast } from "@/components/toast"
+
 export const metadata: Metadata = {
   title: "Lana Bank | Admin Panel",
   icons: [
     {
       rel: "icon",
       url: "/favicon.ico",
+      type: "image/x-icon",
     },
   ],
 }
 
-const RootLayout = async ({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) => {
+const RootLayout: React.FC<React.PropsWithChildren> = async ({ children }) => {
   return (
     <html lang="en">
-      <body className="antialiased w-screen h-screen select-none">{children}</body>
+      <body
+        className={`${HelveticaNeueFont.variable} ${RobotoMono.variable} antialiased w-screen h-screen select-none`}
+      >
+        <Toast />
+        {children}
+      </body>
     </html>
   )
 }
--- apps/admin-panel/app/page.tsx
+++ apps/admin-panel/app/page.tsx
@@ -1,7 +1,16 @@
-import React from "react"
+"use client"
+
+import { toast } from "sonner"
+
+import { Button } from "@/components"
 
 const Page = () => {
-  return <div>PLS WORK</div>
+  return (
+    <>
+      <div>PLS WORK</div>
+      <Button title="Click me" onClick={() => toast.success("WORKS")} />
+    </>
+  )
 }
 
 export default Page
