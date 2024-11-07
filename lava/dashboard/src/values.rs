e6eb9ca89d517078207e1e2b240f891d438065ec
Vaibhav <thevaibhavdixit@gmail.com>
2024-11-07 15:51:32 +0530

    chore: extract out money module

--- lava/dashboard/src/values.rs
+++ lava/dashboard/src/values.rs
@@ -2,13 +2,14 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 use lava_events::*;
+use lava_money::{Satoshis, UsdCents};
 
 #[derive(Clone, Default, Serialize, Deserialize, Debug)]
 pub struct DashboardValues {
     pub active_facilities: u32,
     pub pending_facilities: u32,
-    pub total_disbursed: u64,
-    pub total_collateral: u64,
+    pub total_disbursed: UsdCents,
+    pub total_collateral: Satoshis,
     pub last_updated: DateTime<Utc>,
 }
 
@@ -16,35 +17,40 @@ impl DashboardValues {
     pub(crate) fn process_event(&mut self, recorded_at: DateTime<Utc>, event: &LavaEvent) -> bool {
         self.last_updated = recorded_at;
         match event {
-            LavaEvent::Credit(CreditEvent::CreditFacilityCreated { .. }) => {
+            LavaEvent::Credit(CreditEvent::FacilityCreated { .. }) => {
                 self.pending_facilities += 1;
                 true
             }
-            LavaEvent::Credit(CreditEvent::CreditFacilityActivated { .. }) => {
+            LavaEvent::Credit(CreditEvent::FacilityActivated { .. }) => {
                 self.pending_facilities -= 1;
                 self.active_facilities += 1;
                 true
             }
-            LavaEvent::Credit(CreditEvent::CreditFacilityCompleted { .. }) => {
+            LavaEvent::Credit(CreditEvent::FacilityCompleted { .. }) => {
                 self.active_facilities -= 1;
                 true
             }
-            LavaEvent::Credit(CreditEvent::DisbursalConcluded { amount, .. }) => {
-                self.total_disbursed += amount;
+            LavaEvent::Credit(CreditEvent::DisbursalSettled { amount, .. }) => {
+                self.total_disbursed += *amount;
                 true
             }
-            LavaEvent::Credit(CreditEvent::PaymentRecorded {
+            LavaEvent::Credit(CreditEvent::FacilityRepaymentRecorded {
                 disbursal_amount, ..
             }) => {
-                self.total_disbursed -= disbursal_amount;
+                self.total_disbursed -= *disbursal_amount;
                 true
             }
-            LavaEvent::Credit(CreditEvent::CollateralAdded { amount, .. }) => {
-                self.total_collateral += amount;
-                true
-            }
-            LavaEvent::Credit(CreditEvent::CollateralRemoved { amount, .. }) => {
-                self.total_collateral -= amount;
+            LavaEvent::Credit(CreditEvent::FacilityCollateralUpdated {
+                abs_diff, action, ..
+            }) => {
+                match action {
+                    FacilityCollateralUpdateAction::Add => {
+                        self.total_collateral += *abs_diff;
+                    }
+                    FacilityCollateralUpdateAction::Remove => {
+                        self.total_collateral -= *abs_diff;
+                    }
+                }
                 true
             }
             _ => false,
