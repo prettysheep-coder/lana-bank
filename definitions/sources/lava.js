cafb9fcede383bed9f0fbe5658e28c481b401fd9
Sebastien Verreault <sv@popos>
2024-11-08 15:42:23 +0900

    feat: add interest accrual events

--- definitions/sources/lava.js
+++ definitions/sources/lava.js
@@ -1,4 +1,4 @@
-const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants", "credit_facility_events", "accounts", "account_sets", "account_set_members", "balances"]
+const tables = ["loans", "loan_events", "customer_events", "interest_accrual_events", "price_cents_btc", "sumsub_applicants", "credit_facility_events", "accounts", "account_sets", "account_set_members", "balances"]
 
 envs.all.forEach((env) => {
   tables.forEach((table) => {
