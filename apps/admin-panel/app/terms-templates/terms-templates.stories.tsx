6a1815b020f34407ff509d3238304e8ebb2c2ed4
siddharth <siddharthtiwarikreplind@gmail.com>
2024-11-26 19:00:37 +0530

    chore: adding storybooks for entity pages

---
+++ apps/admin-panel/app/terms-templates/terms-templates.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { MockedProvider } from "@apollo/client/testing"
+import { faker } from "@faker-js/faker"
+
+import TermPage from "./page"
+
+import { TermsTemplatesDocument } from "@/lib/graphql/generated"
+import { mockTermsTemplate } from "@/lib/graphql/generated/mocks"
+
+const createRandomTermsTemplates = () => {
+  const count = faker.number.int({ min: 3, max: 10 })
+  return Array.from({ length: count }, () => mockTermsTemplate())
+}
+
+const baseMocks = [
+  {
+    request: {
+      query: TermsTemplatesDocument,
+    },
+    result: {
+      data: {
+        termsTemplates: createRandomTermsTemplates(),
+      },
+    },
+  },
+]
+
+const meta = {
+  title: "Pages/TermsTemplates",
+  component: TermPage,
+  parameters: {
+    layout: "fullscreen",
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+} satisfies Meta<typeof TermPage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <MockedProvider mocks={baseMocks} addTypename={false}>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+}
