f15a7c14311b1fd9d92fec1891b43ce7fee39be3
Rishi Kumar <rsi.dev17@gmail.com>
2024-11-29 14:52:32 +0530

    chore: Directly stream logs:

--- dev/bin/tilt-ci.sh
+++ dev/bin/tilt-ci.sh
@@ -5,13 +5,11 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 [ -f tmp.env.ci ] && source tmp.env.ci || true
 
 cd "${REPO_ROOT}"
-tilt ci --file dev/Tiltfile | tee tilt.log | grep cypress
+tilt ci --file dev/Tiltfile | tee tilt.log 
 status=${PIPESTATUS[0]}
 
 if [[ $status -eq 0 ]]; then
   echo "Tilt CI passed"
-else
-  cat tilt.log
 fi
 
 exit "$status"
