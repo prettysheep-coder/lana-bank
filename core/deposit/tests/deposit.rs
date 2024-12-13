0e27c694c8adc86ad843d10b9a02203e4f591641
Vaibhav <thevaibhavdixit@gmail.com>
2024-12-14 00:43:28 +0530

    test: dumnmy objects, actions and events for test

--- core/deposit/tests/deposit.rs
+++ core/deposit/tests/deposit.rs
@@ -24,29 +24,175 @@ pub async fn init_journal(cala: &CalaLedger) -> anyhow::Result<cala_ledger::Jour
     Ok(journal.id)
 }
 
+mod action {
+    use deposit::{CoreDepositAction, GovernanceAction};
+
+    #[derive(Clone, Copy, Debug, PartialEq, strum::EnumDiscriminants)]
+    #[strum_discriminants(derive(strum::Display, strum::EnumString))]
+    #[strum_discriminants(strum(serialize_all = "kebab-case"))]
+    pub(super) enum DummyAction {
+        CoreDeposit(CoreDepositAction),
+        Governance(GovernanceAction),
+    }
+
+    impl From<CoreDepositAction> for DummyAction {
+        fn from(action: CoreDepositAction) -> Self {
+            DummyAction::CoreDeposit(action)
+        }
+    }
+
+    impl From<GovernanceAction> for DummyAction {
+        fn from(action: GovernanceAction) -> Self {
+            DummyAction::Governance(action)
+        }
+    }
+
+    impl std::fmt::Display for DummyAction {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}:", DummyActionDiscriminants::from(self))?;
+            use DummyAction::*;
+            match self {
+                CoreDeposit(action) => action.fmt(f),
+                Governance(action) => action.fmt(f),
+            }
+        }
+    }
+
+    impl std::str::FromStr for DummyAction {
+        type Err = strum::ParseError;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let (module, action) = s.split_once(':').expect("missing colon");
+            use DummyActionDiscriminants::*;
+            let res = match module.parse()? {
+                CoreDeposit => DummyAction::from(action.parse::<CoreDepositAction>()?),
+                Governance => DummyAction::from(action.parse::<GovernanceAction>()?),
+            };
+            Ok(res)
+        }
+    }
+}
+
+mod object {
+    use deposit::{CoreDepositObject, GovernanceObject};
+
+    #[derive(Clone, Copy, Debug, PartialEq, strum::EnumDiscriminants)]
+    #[strum_discriminants(derive(strum::Display, strum::EnumString))]
+    #[strum_discriminants(strum(serialize_all = "kebab-case"))]
+    pub(super) enum DummyObject {
+        CoreDeposit(CoreDepositObject),
+        Governance(GovernanceObject),
+    }
+
+    impl From<CoreDepositObject> for DummyObject {
+        fn from(action: CoreDepositObject) -> Self {
+            DummyObject::CoreDeposit(action)
+        }
+    }
+
+    impl From<GovernanceObject> for DummyObject {
+        fn from(action: GovernanceObject) -> Self {
+            DummyObject::Governance(action)
+        }
+    }
+
+    impl std::fmt::Display for DummyObject {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}/", DummyObjectDiscriminants::from(self))?;
+            use DummyObject::*;
+            match self {
+                CoreDeposit(action) => action.fmt(f),
+                Governance(action) => action.fmt(f),
+            }
+        }
+    }
+
+    impl std::str::FromStr for DummyObject {
+        type Err = &'static str;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let (module, object) = s.split_once('/').expect("missing colon");
+            use DummyObjectDiscriminants::*;
+            let res = match module.parse().expect("invalid module") {
+                CoreDeposit => DummyObject::from(object.parse::<CoreDepositObject>()?),
+                Governance => DummyObject::from(object.parse::<GovernanceObject>()?),
+            };
+            Ok(res)
+        }
+    }
+}
+
+mod event {
+    use serde::{Deserialize, Serialize};
+
+    use deposit::CoreDepositEvent;
+    use governance::GovernanceEvent;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(tag = "module")]
+    pub(super) enum DummyEvent {
+        CoreDeposit(CoreDepositEvent),
+        Governance(GovernanceEvent),
+    }
+
+    macro_rules! impl_event_marker {
+        ($from_type:ty, $variant:ident) => {
+            impl outbox::OutboxEventMarker<$from_type> for DummyEvent {
+                fn as_event(&self) -> Option<&$from_type> {
+                    match self {
+                        Self::$variant(ref event) => Some(event),
+                        _ => None,
+                    }
+                }
+            }
+            impl From<$from_type> for DummyEvent {
+                fn from(event: $from_type) -> Self {
+                    Self::$variant(event)
+                }
+            }
+        };
+    }
+
+    impl_event_marker!(GovernanceEvent, Governance);
+    impl_event_marker!(CoreDepositEvent, CoreDeposit);
+}
+
 #[tokio::test]
 async fn deposit() -> anyhow::Result<()> {
     let pool = init_pool().await?;
 
-    let outbox = outbox::Outbox::<CoreDepositEvent>::init(&pool).await?;
-    let authz = authz::dummy::DummyPerms::<CoreDepositAction, CoreDepositObject>::new();
+    let outbox = outbox::Outbox::<event::DummyEvent>::init(&pool).await?;
+    let authz = authz::dummy::DummyPerms::<action::DummyAction, object::DummyObject>::new();
+
+    let governance = governance::Governance::new(&pool, &authz, &outbox);
 
     let cala_config = CalaLedgerConfig::builder()
         .pool(pool.clone())
         .exec_migrations(false)
         .build()?;
     let cala = CalaLedger::init(cala_config).await?;
+    let jobs = job::Jobs::new(&pool, job::JobExecutorConfig::default());
 
     let journal_id = init_journal(&cala).await?;
     let omnibus_code = journal_id.to_string();
 
-    let deposit =
-        CoreDeposit::init(&pool, &authz, &outbox, &cala, journal_id, omnibus_code).await?;
+    let deposit = CoreDeposit::init(
+        &pool,
+        &authz,
+        &outbox,
+        &governance,
+        &jobs,
+        &cala,
+        journal_id,
+        omnibus_code,
+    )
+    .await?;
 
     let account_holder_id = DepositAccountHolderId::new();
     let account = deposit
         .create_account(&DummySubject, account_holder_id)
         .await?;
+
     deposit
         .record_deposit(
             &DummySubject,
