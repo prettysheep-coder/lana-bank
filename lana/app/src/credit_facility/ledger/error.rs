dd7bfb20ebdb144c87ed806b0075d5ad729019c6
Vaibhav <thevaibhavdixit@gmail.com>
2025-01-07 12:39:48 +0530

    fix: error naming

--- lana/app/src/credit_facility/ledger/error.rs
+++ lana/app/src/credit_facility/ledger/error.rs
@@ -2,16 +2,16 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum CreditLedgerError {
-    #[error("DepositLedgerError - Sqlx: {0}")]
+    #[error("CreditLedgerError - Sqlx: {0}")]
     Sqlx(#[from] sqlx::Error),
-    #[error("DepositLedgerError - CalaLedger: {0}")]
+    #[error("CreditLedgerError - CalaLedger: {0}")]
     CalaLedger(#[from] cala_ledger::error::LedgerError),
-    #[error("DepositLedgerError - CalaAccountError: {0}")]
+    #[error("CreditLedgerError - CalaAccountError: {0}")]
     CalaAccount(#[from] cala_ledger::account::error::AccountError),
-    #[error("DepositLedgerError - CalaTxTemplateError: {0}")]
+    #[error("CreditLedgerError - CalaTxTemplateError: {0}")]
     CalaTxTemplate(#[from] cala_ledger::tx_template::error::TxTemplateError),
-    #[error("DepositLedgerError - CalaBalanceError: {0}")]
+    #[error("CreditLedgerError - CalaBalanceError: {0}")]
     CalaBalance(#[from] cala_ledger::balance::error::BalanceError),
-    #[error("DepositLedgerError - ConversionError: {0}")]
+    #[error("CreditLedgerError - ConversionError: {0}")]
     ConversionError(#[from] core_money::ConversionError),
 }
