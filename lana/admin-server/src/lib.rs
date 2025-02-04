521340451fce2107f13ef5b3a5cbd3dcd0838c99
Sandipan Dey <hey@sandipan.dev>
2025-02-04 21:28:45 +0530

    chore: hydrate user id

--- lana/admin-server/src/lib.rs
+++ lana/admin-server/src/lib.rs
@@ -62,7 +62,7 @@ pub async fn run(config: AdminServerConfig, app: LanaApp) -> anyhow::Result<()> 
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AdminJwtClaims {
-    pub sub: String,
+    pub subject: String,
 }
 
 #[instrument(name = "admin_server.graphql", skip_all, fields(error, error.level, error.message))]
@@ -75,7 +75,7 @@ pub async fn graphql_handler(
     tracing_utils::http::extract_tracing(&headers);
     let mut req = req.into_inner();
 
-    match uuid::Uuid::parse_str(&jwt_claims.sub) {
+    match uuid::Uuid::parse_str(&jwt_claims.subject) {
         Ok(id) => {
             let auth_context = AdminAuthContext::new(id);
             req = req.data(auth_context);
