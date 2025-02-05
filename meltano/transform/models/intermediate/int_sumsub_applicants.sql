f5e7725dfd45b068599bbb22e4d7cabe8eb22a45
Jose Ignacio Rojas <jir@galoy.io>
2025-02-05 13:25:31 -0700

    chore: sqlfluff:fix instead of ignoring

--- meltano/transform/models/intermediate/int_sumsub_applicants.sql
+++ meltano/transform/models/intermediate/int_sumsub_applicants.sql
@@ -1,9 +1,9 @@
 select
     customer_id,
     json_value(parsed_content.id) as applicant_id,
-    timestamp(json_value(parsed_content.createdAt)) as created_at,
-    json_value(parsed_content.info.firstName) as first_name,
-    json_value(parsed_content.info.lastName) as last_name,
+    timestamp(json_value(parsed_content.createdat)) as created_at,
+    json_value(parsed_content.info.firstname) as first_name,
+    json_value(parsed_content.info.lastname) as last_name,
     date(json_value(parsed_content.info.dob)) as date_of_birth,
     json_value(parsed_content.info.gender) as gender,
     json_value(parsed_content.info.country) as iso_alpha_3_code,
@@ -11,26 +11,27 @@ select
     array(
         select as struct
             json_value(doc.country) as iso_alpha_3_code,
-            json_value(doc.idDocType) as document_type,
+            json_value(doc.iddoctype) as document_type,
             json_value(doc.number) as number
-        from unnest(json_query_array(parsed_content.info.idDocs)) as doc
+        from unnest(json_query_array(parsed_content.info.iddocs)) as doc
     ) as id_documents,
 
     array(
         select as struct
-            json_value(questions.sections.personalInformation.items.occupation.value)
+            json_value(questions.sections.personalinformation.items.occupation.value)
                 as occupation_code,
-            json_value(questions.sections.personalInformation.items.economicActivity.value)
+            json_value(questions.sections.personalinformation.items.economicactivity.value)
                 as economic_activity_code,
-            json_value(questions.sections.personalInformation.items.countryOfResidence.value)
+            json_value(questions.sections.personalinformation.items.countryofresidence.value)
                 as country_of_residence_iso_alpha_3_code,
             json_value(
-                questions.sections.personalInformation.items.taxIdentificationNum.value
+                questions.sections.personalinformation.items.taxidentificationnum.value
             ) as tax_id_number
         from unnest(json_query_array(parsed_content.questionnaires)) as questions
     ) as questionnaires
 
 from {{ ref('stg_sumsub_applicants') }}
 
-where parsed_content is not null
-	and parsed_content.errorCode is null
+where
+    parsed_content is not null
+    and parsed_content.errorcode is null
