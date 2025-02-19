42c065a0381aa6f0c9afb0e270753102ff84ae61
Vaibhav <thevaibhavdixit@gmail.com>
2025-02-19 15:37:32 +0530

    refactor: move price to core

---
+++ core/price/tests/price.rs
@@ -0,0 +1,10 @@
+use price::Price;
+
+#[tokio::test]
+async fn get_price() -> anyhow::Result<()> {
+    let price = Price::new();
+    let res = price.usd_cents_per_btc().await;
+    assert!(res.is_ok());
+
+    Ok(())
+}
