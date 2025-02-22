f7de1ed1520dbfeff45e02bd903646321748bc90
Vaibhav <thevaibhavdixit@gmail.com>
2025-02-22 23:43:04 +0530

    wip: integrate core/credit

--- lana/app/src/app/mod.rs
+++ lana/app/src/app/mod.rs
@@ -138,7 +138,7 @@ impl LanaApp {
             &governance,
             &jobs,
             &authz,
-            &deposits,
+            &customers,
             &price,
             &outbox,
             charts_init.credit_facilities.factories,
