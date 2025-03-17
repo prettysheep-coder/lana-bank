e771a5eb23d8804d261581d3dfc80093d176c1e2
bodymindarts <justin@galoy.io>
2025-03-17 15:45:30 +0100

    chore: add DepositPublisher

--- core/deposit/src/event.rs
+++ core/deposit/src/event.rs
@@ -1,7 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+use super::{DepositId, WithdrawalId};
+use core_money::UsdCents;
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum CoreDepositEvent {
-    DepositAccountCreated,
+    DepositInitialized { id: DepositId, amount: UsdCents },
+    WithdrawalConfirmed { id: WithdrawalId, amount: UsdCents },
 }
