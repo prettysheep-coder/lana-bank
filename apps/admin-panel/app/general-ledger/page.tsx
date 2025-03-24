8e0822c4bf892e852836b8f2498a8dcc571ef454
Sandipan Dey <hey@sandipan.dev>
2025-03-24 23:19:54 +0530

    chore: general ledger nav item in admin panel

---
+++ apps/admin-panel/app/general-ledger/page.tsx
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client"
+import { useTranslations } from "next-intl"
+
+gql`
+`
+
+const GeneralLedgerPage: React.FC = () => {
+        const t = useTranslations("GeneralLedger")
+
+        return <></>
+}
+
+export default GeneralLedgerPage
