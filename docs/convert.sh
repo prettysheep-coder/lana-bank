f4cc4f9880e684045fdb143ce4bb5e416b54b874
Nicolas Burtey <nb@galoy.io>
2025-03-27 15:29:14 -0600

    chore: install node related package locally with pnpm

--- docs/convert.sh
+++ docs/convert.sh
@@ -1,7 +1,5 @@
-# npx -p @anthropic-ai/claude-code 
-# cat .claude/commands/mermaid.md | claude --dangerously-skip-permissions -p
-
+# cat .claude/commands/mermaid.md | pnpm claude --dangerously-skip-permissions -p
 # cp ../../slidev/pages/*.md slidev/
 
-npx -p @mermaid-js/mermaid-cli mmdc -i core-banking-manual.md -o core-banking-manual-svg.md
-npx -p md-to-pdf md-to-pdf core-banking-manual-svg.md
+pnpm mmdc -i core-banking-manual.md -o core-banking-manual-svg.md
+pnpm md-to-pdf core-banking-manual-svg.md
