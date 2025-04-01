7709b994ecb19461192f5e01e62f489e5faf87c8
sebastienverreault <sv@galoy.io>
2025-04-01 21:29:25 +0900

    fix: null types

--- meltano/transform/models/intermediate/el-salvador-ssf/int_nrsf_03_01_cliente.sql
+++ meltano/transform/models/intermediate/el-salvador-ssf/int_nrsf_03_01_cliente.sql
@@ -18,16 +18,16 @@ customers as (
 select
     left(replace(customer_id, '-', ''), 14) as `NIU`,
     first_name as `Primer Nombre`,
-    null as `Segundo Nombre`,
-    null as `Tercer Nombre`,
+    cast(null as string) as `Segundo Nombre`,
+    cast(null as string) as `Tercer Nombre`,
     last_name as `Primer Apellido`,
-    null as `Segundo Apellido`,
-    null as `Apellido de casada`,
-    null as `Razón social`,
+    cast(null as string) as `Segundo Apellido`,
+    cast(null as string) as `Apellido de casada`,
+    cast(null as string) as `Razón social`,
     '1' as `Tipo de persona`,
-    nationality_code as `Nacionalidad`,
-    economic_activity_code as `Actividad Económica`,
-    country_of_residence_code as `País de Residencia`,
+    cast(nationality_code as string) as `Nacionalidad`,
+    cast(economic_activity_code as string) as `Actividad Económica`,
+    cast(country_of_residence_code as string) as `País de Residencia`,
     '15' as `Departamento`,
     '00' as `Distrito`,
     formatted_address as `Dirección`,
@@ -42,7 +42,7 @@ select
     '{{ npb4_17_03_tipos_de_categorias_de_riesgo('Deudores normales') }}'
         as `Clasificación de Riesgo`,
     relationship_to_bank as `Tipo de relación`,
-    null as `Agencia`,
+    cast(null as string) as `Agencia`,
     least(sum_total_collateral, {{ var('deposits_coverage_limit') }}) as `Saldo garantizado`
 from
     customers
