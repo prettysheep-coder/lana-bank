9dbbe68094b10b2c5678503edc7d16faa7cb37cf
bodymindarts <justin@galoy.io>
2025-04-08 11:14:18 +0200

    fix: fmt

--- core/accounting/tests/manual_transaction.rs
+++ core/accounting/tests/manual_transaction.rs
@@ -2,7 +2,7 @@ mod helpers;
 
 use authz::dummy::DummySubject;
 use cala_ledger::{CalaLedger, CalaLedgerConfig, Currency, DebitOrCredit};
-use core_accounting::{manual_transactions::AccountIdOrCode, CoreAccounting, ManualEntryInput};
+use core_accounting::{CoreAccounting, ManualEntryInput, manual_transactions::AccountIdOrCode};
 use helpers::{action, object};
 use rust_decimal_macros::dec;
 
