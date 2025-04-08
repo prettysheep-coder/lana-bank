1ceafcd5f11e9e050bd32d11cfe24a3370cb34af
bodymindarts <justin@galoy.io>
2025-04-08 11:04:06 +0200

    test: assert transaction result

--- core/accounting/tests/manual_transaction.rs
+++ core/accounting/tests/manual_transaction.rs
@@ -2,13 +2,13 @@ mod helpers;
 
 use authz::dummy::DummySubject;
 use cala_ledger::{CalaLedger, CalaLedgerConfig, Currency, DebitOrCredit};
-use core_accounting::{CoreAccounting, ManualEntryInput, manual_transactions::AccountIdOrCode};
+use core_accounting::{manual_transactions::AccountIdOrCode, CoreAccounting, ManualEntryInput};
 use helpers::{action, object};
 use rust_decimal_macros::dec;
 
 #[tokio::test]
 #[rustfmt::skip]
-async fn manual_transaction_with_two_entries() -> anyhow::Result<()> {
+async fn manual_transaction() -> anyhow::Result<()> {
     use rand::Rng;
     let pool = helpers::init_pool().await?;
     let cala_config = CalaLedgerConfig::builder().pool(pool.clone()).exec_migrations(false).build()?;
@@ -26,15 +26,6 @@ async fn manual_transaction_with_two_entries() -> anyhow::Result<()> {
     let chart_id = chart.id;
     let _ = accounting.chart_of_accounts().import_from_csv(&DummySubject, chart_id, import).await?;
 
-    let assets = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "1".to_string()).await?.unwrap();
-    let liabilities = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "2".to_string()).await?.unwrap();
-
-    // account sets in chart of accounts have no members
-    let assets_members = cala.account_sets().list_members_by_created_at(assets.id.into(), Default::default()).await?.entities;
-    let liabilities_members = cala.account_sets().list_members_by_created_at(liabilities.id.into(), Default::default()).await?.entities;
-    assert!(assets_members.is_empty());
-    assert!(liabilities_members.is_empty());
-
     let to: AccountIdOrCode = "1".parse().unwrap();
     let from: AccountIdOrCode = "2".parse().unwrap();
 
@@ -44,25 +35,8 @@ async fn manual_transaction_with_two_entries() -> anyhow::Result<()> {
     ];
     accounting.execute_manual_transaction(&DummySubject, &chart_ref, None, "Test transaction 1".to_string(), entries).await?;
 
-    // each account set in chart of accounts has one new member
-    let assets_members_1 = cala.account_sets().list_members_by_created_at(assets.id.into(), Default::default()).await?.entities;
-    let liabilities_members_1 = cala.account_sets().list_members_by_created_at(liabilities.id.into(), Default::default()).await?.entities;
-    assert_eq!(assets_members_1.len(), 1);
-    assert_eq!(liabilities_members_1.len(), 1);
-
-    let entries = vec![
-        ManualEntryInput::builder().account_id_or_code(to).amount(dec!(200)).currency(Currency::USD).direction(DebitOrCredit::Debit).description("test 2 debit").build().unwrap(),
-        ManualEntryInput::builder().account_id_or_code(from).amount(dec!(200)).currency(Currency::USD).direction(DebitOrCredit::Credit).description("test 2 credit").build().unwrap(),
-    ];
-    accounting.execute_manual_transaction(&DummySubject, &chart_ref, None, "Test transaction 2".to_string(), entries).await?;
-
-    // members of account set in chart of accounts did not change
-    let assets_members_2 = cala.account_sets().list_members_by_created_at(assets.id.into(), Default::default()).await?.entities;
-    let liabilities_members_2 = cala.account_sets().list_members_by_created_at(liabilities.id.into(), Default::default()).await?.entities;
-    assert_eq!(assets_members_2.len(), 1);
-    assert_eq!(liabilities_members_2.len(), 1);
-    assert_eq!(assets_members_1[0].id, assets_members_2[0].id);
-    assert_eq!(liabilities_members_1[0].id, liabilities_members_2[0].id);
+    let account = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "2".to_string()).await?.unwrap();
+    assert_eq!(account.usd_balance_range.expect("should have balance").end.expect("balance missing").settled(), dec!(100));
 
     Ok(())
 }
