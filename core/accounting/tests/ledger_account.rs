4edcfb4b4bc89c506f775833acd83530e9bee1a5
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-09 19:00:13 +0530

    test: ledger account children

--- core/accounting/tests/ledger_account.rs
+++ core/accounting/tests/ledger_account.rs
@@ -67,3 +67,59 @@ async fn ledger_account_ancestors() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+#[rustfmt::skip]
+async fn ledger_account_children() -> anyhow::Result<()> {
+    use rand::Rng;
+    let pool = helpers::init_pool().await?;
+    let cala_config = CalaLedgerConfig::builder().pool(pool.clone()).exec_migrations(false).build()?;
+    let cala = CalaLedger::init(cala_config).await?;
+    let authz = authz::dummy::DummyPerms::<action::DummyAction, object::DummyObject>::new();
+    let journal_id = helpers::init_journal(&cala).await?;
+
+    let accounting = CoreAccounting::new(&pool, &authz, &cala, journal_id);
+    let chart_ref = format!("ref-{:08}", rand::thread_rng().gen_range(0..10000));
+    let chart = accounting.chart_of_accounts().create_chart(&DummySubject, "Test chart".to_string(), chart_ref.clone()).await?;
+    let import = r#"
+        1,,Root
+        11,,Child
+        11,1,Grandchild
+        "#;
+    let chart_id = chart.id;
+    accounting.chart_of_accounts().import_from_csv(&DummySubject, chart_id, import).await?;
+
+    let root = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "1".to_string()).await?.unwrap();
+    let child = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "11".to_string()).await?.unwrap();
+    let grandchild = accounting.find_ledger_account_by_code(&DummySubject, &chart_ref, "11.1".to_string()).await?.unwrap();
+
+    // chart of account
+    assert_eq!(root.children_ids, vec![child.id]);
+    assert_eq!(child.children_ids, vec![grandchild.id]);
+    assert_eq!(grandchild.children_ids, vec![]);
+
+    let internal_id = AccountSetId::new();
+    cala.account_sets()
+        .create(NewAccountSet::builder().id(internal_id).name("Internal").journal_id(journal_id).build().unwrap())
+        .await?;
+    cala.account_sets().add_member(grandchild.id.into(), internal_id).await?;
+    // should be empty
+    assert_eq!(grandchild.children_ids, vec![]);
+
+    let leaf_id = AccountId::new();
+    cala.accounts()
+        .create(NewAccount::builder().id(leaf_id).code(leaf_id.to_string()).name("Leaf").build().unwrap())
+        .await?;
+    cala.account_sets().add_member(internal_id, leaf_id).await?;
+
+    // internal account
+    let ledger_account = accounting.find_ledger_account_by_id(&DummySubject, &chart_ref, internal_id).await?.unwrap();
+    assert_eq!(ledger_account.children_ids, vec![leaf_id.into()]);
+
+    // leaf account with internal
+    let ledger_account = accounting.find_ledger_account_by_id(&DummySubject, &chart_ref, leaf_id).await?.unwrap();
+    // leaf should be empty
+    assert_eq!(ledger_account.children_ids, vec![]);
+
+    Ok(())
+}
