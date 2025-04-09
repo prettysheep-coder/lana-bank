2564ed6da98562a17981218610b6861138e428b1
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-10 00:01:20 +0530

    fix: clippy

--- lana/admin-server/src/graphql/accounting/mod.rs
+++ lana/admin-server/src/graphql/accounting/mod.rs
@@ -2,13 +2,10 @@ mod journal_entry;
 mod ledger_account;
 mod ledger_transaction;
 mod manual_transaction;
+mod profit_and_loss;
 
 pub use journal_entry::*;
 pub use ledger_account::*;
 pub use ledger_transaction::*;
 pub use manual_transaction::*;
-mod profit_and_loss;
-
-pub use journal_entry::*;
-pub use ledger_account::*;
 pub use profit_and_loss::*;
