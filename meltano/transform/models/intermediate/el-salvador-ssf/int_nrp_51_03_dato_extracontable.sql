b0ab5ef5068a25ee1e4b3f20977589a9902f29a4
sebastienverreault <sv@galoy.io>
2025-04-14 19:01:04 +0900

    feat: nrp-51 off balance sheet report, no source

--- meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_51_03_dato_extracontable.sql
+++ meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_51_03_dato_extracontable.sql
@@ -1,95 +1,22 @@
 with
 
-accounts as (
+off_balances as (
     select *
-    from
-        {{ ref('stg_accounts') }}
+    from {{ ref('int_account_balances') }}
+    where 1 = 0
+
 )
 ,
 
-account_sets as (
-    select *
-    from
-        {{ ref('stg_account_sets') }}
-)
-,
+final as (
 
-account_set_member_accounts as (
     select *
-    from
-        {{ ref('stg_account_set_member_accounts') }}
+    from off_balances
 )
-,
-
-account_set_member_account_sets as (
-    select *
-    from
-        {{ ref('stg_account_set_member_account_sets') }}
-)
-,
-
-account_balances as (
-    select *
-    from
-        {{ ref('stg_account_balances') }}
-)
-,
-
-bitfinex_order_book as (
-    select *
-    from
-        {{ ref('stg_bitfinex_order_book') }}
-)
-,
-
-bitfinex_ticker_price as (
-    select *
-    from
-        {{ ref('stg_bitfinex_ticker_price') }}
-)
-,
-
-bitfinex_trades as (
-    select *
-    from
-        {{ ref('stg_bitfinex_trades') }}
-)
-,
-
-credit_facility_events as (
-    select *
-    from
-        {{ ref('stg_credit_facility_events') }}
-)
-,
-
-customer_events as (
-    select *
-    from
-        {{ ref('stg_customer_events') }}
-)
-,
-
-sumsub_applicants as (
-    select *
-    from
-        {{ ref('stg_sumsub_applicants') }}
-)
-
 
 select
     'TODO' as `id_codigo_extracontable`,
     'TODO' as `desc_extra_contable`,
     7060.0 as `Valor`
--- FROM
---     accounts,
---     account_sets,
---     account_set_member_accounts,
---     account_set_member_account_sets,
---     account_balances,
---     bitfinex_order_book,
---     bitfinex_ticker_price,
---     bitfinex_trades,
---     credit_facility_events,
---     customer_events,
---     sumsub_applicants,
+from
+    final
